67bd37135690ff0dd1f937a45653dcf79d55029d
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-06-16 14:20:51 +0300

    feat: name inputs, game rounds logic
    Adds a prompt for the two players' names and a message after the round is
    finished with two options: play again with the same names or choose new
    names and play.

--- scripts/index.js
+++ scripts/index.js
@@ -20,16 +20,20 @@ const board = (function () {
 
 const game = (function () {
   let currentMarker = 'X';
+  let roundNumber = 1;
   let winner = null;
-  let active = true;
+  let active = false;
 
   const playRound = (cellNumber) => {
     board.setMarker(cellNumber, currentMarker);
     const winnerMarker = checkWinnerMarker();
     if(winnerMarker !== '') {
       setWinner(winnerMarker);
-      end();
+      end(winner);
+    } else if(roundNumber === 9) {
+      end('draw');
     } else {
+      roundNumber++;
       if(currentMarker === 'X')
         currentMarker = 'O';
       else
@@ -59,25 +63,37 @@ const game = (function () {
       winner = playerTwo;
   }
 
-  const end = () => {
-    reset();
+  const end = (result) => {
+    game.active = false;
+    endDialogDOM.show();
+    if(result === 'draw') {
+      winnerTextDOM.textContent = "It's a draw!";
+    } else {
+      winnerTextDOM.textContent = `${winner.name} wins!`;
+    }
   }
 
-  const reset = () => {
+  const reset = (keepNames) => {
+    if(keepNames === true) {
+      game.active = true;
+    } else {
+      game.active = false;
+      inputPlayerOneDOM.value = '';
+      inputPlayerTwoDOM.value = '';
+      startDialogDOM.show();
+    }
     game.currentMarker = 'X';
+    roundNumber = 1;
     winner = null;
     board.reset();
   }
 
-  return { active, playRound };
+  return { active, playRound, reset };
 })();
 
 function createPlayer(name) {
   return { name };
 }
-
-const playerOne = createPlayer('A');
-const playerTwo = createPlayer('B');
 
 const cellsDOM = document.querySelectorAll('.cell');
 
@@ -87,3 +103,36 @@ cellsDOM.forEach((cell, index) => cell.addEventListener('click', e => {
     game.playRound(index);
   }
 }));
+
+const formDOM = document.querySelector('form');
+const inputPlayerOneDOM = document.querySelector('.player-one');
+const inputPlayerTwoDOM = document.querySelector('.player-two');
+const playButtonDOM = document.querySelector('.play-button');
+const startDialogDOM = document.querySelector('.start-dialog');
+const endDialogDOM = document.querySelector('.end-dialog');
+const winnerTextDOM = document.querySelector('.winner-text');
+const keepNamesDOM = document.querySelector('.keep-names');
+const newNamesDOM = document.querySelector('.new-names');
+
+let playerOne = null;
+let playerTwo = null;
+
+formDOM.addEventListener('submit', (e) => {
+  e.preventDefault();
+  playerOne = createPlayer(inputPlayerOneDOM.value);
+  playerTwo = createPlayer(inputPlayerTwoDOM.value);
+  game.active = true;
+  startDialogDOM.close();
+});
+
+keepNamesDOM.addEventListener("click", (e) => {
+  e.preventDefault();
+  endDialogDOM.close();
+  game.reset(true);
+});
+
+newNamesDOM.addEventListener("click", (e) => {
+  e.preventDefault();
+  endDialogDOM.close();
+  game.reset(false);
+});
